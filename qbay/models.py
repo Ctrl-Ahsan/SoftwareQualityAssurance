121da95251956523bd7f23f3ddb2b61b55a2bc62
tylerpitcher <tylerpitch2015@gmail.com>
2021-10-07 21:31:09 -0400

    Fixed models

--- qbay/models.py
+++ qbay/models.py
@@ -15,39 +15,49 @@ class User(db.Model):
     username = db.Column(db.String(25), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    wallet = db.Column(db.Float, nullable=False, default=0.0)
-    posts = db.relationship('Post', backref='seller', lazy=True)
+    balance = db.Column(db.Float, nullable=False, default=0.0)
+    shipping_address = db.Column(db.Text, nullable=False)
+    postal_code = db.Column(db.String(6), nullable=False)
+    posts = db.relationship('Product', backref='owner', lazy=True)
     reviews = db.relationship('Review', backref='author', lazy=True)
     
     def __repr__(self):
         return '<User %r>' % self.username
-        
+
+
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    rating = db.Column(db.Float)
-    description = db.Column(db.Text, nullable=False)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_email = db.Column(db.String(25), unique=False, nullable=False)
+    score = db.Column(db.Integer)
+    review = db.Column(db.Text, nullable=False)
     
     def __repr__(self):
-        return '<Review %r>' % self.reviewId
-        
+        return '<Review %r>' % self.id
+
+
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_email = db.Column(db.String(120), nullable=False, unique=False)
     seller = db.Column(db.String(25), nullable=False)
     buyer = db.Column(db.String(25), nullable=False)
-    amount = db.Column(db.Float, nullable=False, default=0.0)
-    
+    price = db.Column(db.Float, nullable=False, default=0.0)
+    product_id = db.Column(db.Integer, nullable=False)
     def __repr__(self):
-        return '<Transaction %r>' % self.transactionId
+        return '<Transaction %r>' % self.id
 
-class Post(db.Model):
+
+class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    seller = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    title = db.Column(db.Text, unique=True, nullable=False)
+    owner = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    owner_email = db.Column(db.String(120), nullable=False)
+    title = db.Column(db.Text, unique=True, nullable=False )
     description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
-        return '<Post %r>' % self.postId
+        return '<Post %r>' % self.id
 
 
 # create all tables
