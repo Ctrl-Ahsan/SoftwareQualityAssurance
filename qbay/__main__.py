6c8cf9766d0810ab6f9f2a075d2d0391231f36e9
tylerpitcher <tylerpitch2015@gmail.com>
2021-10-18 18:46:47 -0400

    Initialized main cli

--- qbay/__main__.py
+++ qbay/__main__.py
@@ -1,21 +1,33 @@
 from qbay import *
-from qbay.cli import login_page, regsiter_page
-
+from qbay.cli import login_page, register_page
 
 def main():
+    user = None
     while True:
-        selection = input(
-            'Welcome. Please type 1 to login. Or type 2 register.')
-        selection = selection.strip()
-        if selection == '1':
-            user = login_page()
-            if user:
-                print(f'welcome {user.username}')
-                break
-            else:
-                print('login failed')
-        elif selection == '2':
-            regsiter_page()
+        if user is None:
+            selection = input(
+                'Welcome. Please type 1 to login. Or 2 register.\n')
+            selection = selection.strip()
+            if selection == '1':
+                user = login_page()
+                if user:
+                    print(f'welcome {user.username}')
+                    continue
+                else:
+                    print('login failed')
+            elif selection == '2':
+                register_page()
+        else: 
+            selection = input (
+                'Please type 1 to go to homepage. Or 2 to update profile.')
+            selection = selection.strip()
+
+            if selection == '1':
+                # Home page here
+                continue
+            elif selection == '2':
+                # Update profile here
+                continue
 
 
 if __name__ == '__main__':
--- qbay/cli.py
+++ qbay/cli.py
@@ -1,6 +1,18 @@
+from qbay.models import login, register
+
 def login_page():
-    pass
+    email = input('Please input email: ')
+    password = input('Please input password: ')
+    return login(email, password)
 
 
 def register_page():
-    pass
+    email = input('Please input email: ')
+    password = input('Please input password: ')
+    password_twice = input('Please input the password again: ')
+    if password != password_twice:
+        print('password entered not the same')
+    elif register('default name', email, password):
+        print('registration succeeded')
+    else:
+        print('regisration failed.')
