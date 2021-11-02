90f753852b82363d3f3c77792edb6fa9f692fab7
tylerpitcher <tylerpitch2015@gmail.com>
2021-11-01 20:01:50 -0400

    Added code to .py

---
+++ qbay_test/frontend/test_create_product/test_create_product.py
@@ -0,0 +1,30 @@
+from os import popen
+from pathlib import Path
+import subprocess
+
+# get expected input/output file
+current_folder = Path(__file__).parent
+
+
+# read expected in/out
+expected_in = open(current_folder.joinpath(
+    'test_login.in'))
+expected_out = open(current_folder.joinpath(
+    'test_login.out')).read()
+
+print(expected_out)
+
+
+def test_login():
+    """capsys -- object created by pytest to 
+    capture stdout and stderr"""
+
+    # pip the input
+    output = subprocess.run(
+        ['python', '-m', 'qbay'],
+        stdin=expected_in,
+        capture_output=True,
+    ).stdout.decode()
+
+    print('outputs', output)
+    assert output.strip() == expected_out.strip()
