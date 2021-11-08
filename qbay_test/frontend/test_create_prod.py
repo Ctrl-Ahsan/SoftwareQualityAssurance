d6545da5ba6ba05980e864b551258cef9b8e3525
Ahsan <theahsan_one@hotmail.com>
2021-11-08 00:35:05 -0500

    Upadted R4_7 again

--- qbay_test/frontend/test_create_prod.py
+++ qbay_test/frontend/test_create_prod.py
@@ -4,6 +4,8 @@ from qbay_test.conftest import base_url
 from unittest.mock import patch
 from qbay.models import User
 import time
+import random
+import string
 from random import randint
 
 
@@ -39,7 +41,7 @@ class FrontEndHomePageTest(BaseCase):
         # partition 2
         self.open(base_url + '/create')
         self.type('#title', 'testing2')
-        self.type('#description', 'T3SjtH6cs2Co4P4tUDRLvXaOCSzlm1H4Av2JlL0oOkKdt7plGTfwsKUfjDoSboyTf6XqVppxaPqeKndmafYSBC7xObDda7hg0LFha9x1dD004y0hCpdX4CvTEfWC80GbuzBnSvfN7bylnX2aKeGFeB9IeJwiz9foCpkrzEEGFynBfBQmTLPLcGFB19YTmR1LpXFKznJKK3OXmEgc7ILoWcnyyi1C0lFO92RXI89KvacNQejKwtAWm46WBjkwtHHJOn8b64M0pp7jMb8kfpSHr6vnJysOUvB2fKidJ0w6IghOxxd7eVvvux7bDB2yKJNyLHz5D6wXQmN88uEQaHSN8HIlkqp3qhw40OsWwJjIrIMArTkgKi7WxLRBKyBaeMfVVsGeFVWmFK34gVitIgxDpRZLpkW6CMytGw2obv64ZF7AkEoDaaLrBQc4xputBf41qRQTo9f9Y2d9P8GtrtPWfwQrndADrORx3w38NALRP4k8S6bc2oOTqSnzIbiRN8ZjkhD7EoTTM3BeCcwRQOVHv1I6hZ6sRj51Q0lstWU5GQx2yZDpBxxo9UVrD8c7fDNjpSkLXSFIYVQDGJIYHYSsfFKMR3MRD1VKAPaBXhRaTRhwA9eweXVxnMyxQ0CvZhiElPvPQAScSrrEtlvfwxxUjA9c0mX0CqNi6mBTIocS3GnucC5oqGPYWIZg2kqsAPkwsOYrYGh21pW19jqwihXKVHHYN6TmPHgBTxxxODDw3jLvYFEbjd8oROkDgbQLcvsEqTRq0JKIlO16u9gjSywRwyBkPeOMfwK4XWB9Xe5RQuGvNegQHWrZH3wV83mNcqjfdpLabUAdjFwdC5k2s72yDucaLvyuUtcwHDrsDzb2A5vmFo33AzOJ7nmQ4HEJvieKZ7htwAvsw2EuuluRk7dzdHTABDLw0nleG46WIgrNhEKnOqMyv641lR9XeOeGH90p3bTXjf69xPtPtIvEXza8WwC9L2eODYMiXJhFJVP8eapq8c9IWkxeN2w4DNXUF9wSpJNYLmShFwJJzV5LfYChzR3xqJRs5w6bH0SDYn8jJnw45Bh0vZ4HLXDqkMHi11513NhVw7aai0SJmb2ulA04vamsE32jsLx3UVB8VcbaFlp3CGkoJltu5V0f5DMmTCd7YC1rbmJpju4Yqcvvb9Q0CHt3iShqjQjMzzehDH7hkgyRaTtRTUWD34mFIy8Zuf4dNRpiPpmw85CnVEcihZz9DtBnZKvsrSjNjnStao8wRlATAXGXVZze5TIW6OHao2jswwIU4SW8RVABXLimF78IGzbagPqLCJbzwL3XxLUAdL9V8C9tUudOWJdz1lcgw5RoiKy2gOA9KuCHsmMvweFvX1MmfkaebwFGODfQtDmk46Z4HUM1XCIUzEZlu3mro0lucwTO6cJeoE5Ej5SGoIocKm75Mu1EttBIZa9Xcd8ulOqftmnW97t3r3B7i2Bsqpb6nY0tB4L6pJvnixe4CB4SU7BWS75AXpMzwYZeGAruGAeZQbZPEBs09gWeenODFQ70bgQt45BmEW0oFxcGOR26cHXrBkDFotYttBjfgvqW5dlLHSJG1saY9iBeZKlUmDXZaaY7s1AxuC0Y4m4cGGrdNq378NV6kfiNX2QbCwAsKIafqrBrozo8aGZDH503oCujQDilAyZrQiMygvzI5llswHLw8atNgYpyEYzvUriuAyYuw025Jo5Vb8ibVg3TUj0V3AAMNCInz0Ecoy3TJ9cAEKbLN0ZkbUPnewFqLexd3fDVTycRsHygYrE0JhNKWDHYydMmFq7s0MZ3HdRcCwTXokveMDAa4bTXc1JPkhLixPX4C7dAMnyM49OLnssmzxDHHo5LTG8vYfvKQAXjctNYuZ9TNmVnbyAvQ2kNKwG1NdFgde5WlnqQ0vmoyFkd8K4Bm79K0bVwCPDJ3cuYqgmyyucY2yLOElodHuBQyrG1snslWsAPbXfKpT5PHj90rjcbbEXg5UYq3F0UN7EC4')
+        self.type('#description', ''.join(random.choice(string.lowercase) for x in range(2001)))
         self.type('#price', '10')
         self.click('input[type=\'submit\']')
         self.assert_element('#message')
@@ -126,10 +128,16 @@ class FrontEndHomePageTest(BaseCase):
         
         # case 1
         self.open(base_url + '/create')
-        self.assert_equal(self.get_current_url, base_url + '/login')
+        self.type('#title', 'something')
+        self.type('#description', 'super something')
+        self.type('#price', 10)
+        self.click('input[type=\'submit\']')
+
+        self.assert_equal(self.get_current_url, base_url + '\login')
 
         # case 2
-        #login
+
+        # login
         self.type('#email', 'raptor@test.com')
         self.type('#password', '123Ab#')
         self.click('input[type=\'submit\']')
@@ -140,7 +148,10 @@ class FrontEndHomePageTest(BaseCase):
         self.type('#price', 10)
         self.click('input[type=\'submit\']')
 
-        self.assert_equal(self.get_current_url, base_url + '/login')
+        # test that it didn't fail
+        self.open(base_url)
+        self.assert_element('h2')
+        self.assert_text('Welcome raptor !', 'h2')
 
     
     def test_r4_8(self, *_):
@@ -175,6 +186,3 @@ class FrontEndHomePageTest(BaseCase):
 
         self.assert_element('#message')
         self.assert_text('Product not created', '#message')
-
-
-
