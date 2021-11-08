9c96b8d7e5b44a688614cb82eb85ff7a2af80f65
Ahsan <theahsan_one@hotmail.com>
2021-11-07 22:23:03 -0500

    Added R1_6 to register. Added R4_4/8 to create product

--- qbay_test/frontend/test_create_prod.py
+++ qbay_test/frontend/test_create_prod.py
@@ -9,6 +9,38 @@ from random import randint
 
 class FrontEndHomePageTest(BaseCase):
 
+    def test_r4_4(self, *_):
+        # open register page
+        self.open(base_url + '/register')
+        self.type('#email', 'short@test.com')
+        self.type('#name', 'short')
+        self.type('#password', '123Ab#')
+        self.type('#password2', '123Ab#')
+        self.click('input[type=\'submit\']')
+
+        # open login
+        self.type('#email', 'short@test.com')
+        self.type('#password', '123Ab#')
+        self.click('input[type=\'submit\']')
+
+        # partition 1
+        self.open(base_url + '/create')
+        self.type('#title', 'temp')
+        self.type('#description', 'tempatempatempatempatempa')
+        self.type('#price', '10')
+        self.click('input[type=\'submit\']')
+        self.assert_element('#temp')
+        self.assert_text('name: temp price: 10.0')
+
+        # partition 2
+        self.open(base_url + '/create')
+        self.type('#title', 'temptemptemptemptemptemptemptemp')
+        self.type('#description', 'tempatempatempatempatempa')
+        self.type('#price', '10')
+        self.click('input[type=\'submit\']')
+        self.assert_element('#message')
+        self.assert_text('Product not created.')
+    
     def test_r4_5(self, *_):
         '''
         Shotgun testing
@@ -43,5 +75,39 @@ class FrontEndHomePageTest(BaseCase):
         self.open(base_url)
         self.assert_element('h2')
         self.assert_text('Welcome upupey !', 'h2')
+    
+    def test_r4_8(self, *_):
+        # open register page
+        self.open(base_url + '/register')
+        self.type('#email', 'sametitle@test.com')
+        self.type('#name', 'same')
+        self.type('#password', '123Ab#')
+        self.type('#password2', '123Ab#')
+        self.click('input[type=\'submit\']')
+
+        self.type('#email', 'sametitle@test.com')
+        self.type('#password', '123Ab#')
+        self.click('input[type=\'submit\']')
+
+        # partition 1
+        self.open(base_url + '/create')
+        self.type('#title', 'abcb')
+        self.type('#description', 'abcbbabcbbabcbbabcbbabcbbabcbb')
+        self.type('#price', 10)
+        self.click('input[type=\'submit\']')
+
+        self.assert_element('#abcb')
+        self.assert_text('name: abcb price: 10.0')
+
+        # partition 2
+        self.open(base_url + '/create')
+        self.type('#title', 'abcb')
+        self.type('#description', 'abcbbabcbbabcbbabcbbabcbbabcbb')
+        self.type('#price', 10)
+        self.click('input[type=\'submit\']')
+
+        self.assert_element('#message')
+        self.assert_text('Product not created', '#message')
+
 
 
