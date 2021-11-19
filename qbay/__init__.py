efc5b595c85ff51cacbd2dfae32d7894b3339dcd
tylerpitcher <tylerpitch2015@gmail.com>
2021-11-19 11:25:34 -0500

    Added files

--- qbay/__init__.py
+++ qbay/__init__.py
@@ -14,6 +14,11 @@ templates = os.path.join(
 )
 
 app = Flask(__name__)
+db_string = os.getenv('db_string')
+if db_string:
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_string
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db.sqlite'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '69cae04b04756f65eabcd2c5a11c8c24'
